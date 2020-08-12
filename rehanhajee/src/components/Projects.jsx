03a0144b53d028260a758edd4d1f773b7466e214
Rehan Hajee <rehanhajee@gmail.com>
2020-08-12 14:36:32 -0400

    Minor modifications

--- rehanhajee/src/components/Projects.jsx
+++ rehanhajee/src/components/Projects.jsx
@@ -27,16 +27,15 @@ class Projects extends Component {
 							<header><h1><u>Projects</u></h1></header>
 							
 							
-
+							{iframe("https://www.cs.ryerson.ca/~r2hajee/personalWebsite/weatherApp/index.html", "Lab 2")}
+							
+							
 							<article>
 							
 							</article>
 							<article>
 								
 							</article>
-
-							{iframe("https://www.cs.ryerson.ca/~r2hajee/Lab2/index.html", "Lab 2")}
-
 							<article>
 							Created a website with Laravel and React showing installation steps for these frameworks, created a Java program which displayed data from a personally created SQL database, created a daily appointment calendar in Java, which includes a partial contact management system
 							</article>

741441b2f4ca92f422fa73e06ac489f367ccab56
Rehan Hajee <rehanhajee@gmail.com>
2020-10-21 15:01:22 -0400

    Updated for Firebase

--- rehanhajee/src/index.js
+++ rehanhajee/src/index.js
@@ -1,12 +1,23 @@
-import 'react-app-polyfill/ie11';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+import App from './App';
+import Firebase, { FirebaseContext } from './components/Firebase';
+
+import 'react-app-polyfill/ie11';
+
+ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </FirebaseContext.Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

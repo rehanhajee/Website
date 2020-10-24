7f7c531c94f247376293a9de9f5087481fedebbf
Rehan Hajee <rehanhajee@gmail.com>
2020-10-24 16:56:37 -0400

    Added alt text for images

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,26 +29,26 @@ class Contact extends Component {
 									<br />
 									<section>
 										<br />
-										<Image className="socialMedia" src={require('../images/facebook-brands.svg')} />
+										<Image className="socialMedia" src={require('../images/facebook-brands.svg')} alt="Facebook logo" />
 										<Button variant="link" href="https://www.facebook.com/rehan.hajee"><h4><b>FB/rehan.hajee</b></h4></Button>
 										<br />
 									</section>
 									<section>
 										<br />
-										<Image className="socialMedia" src={require('../images/linkedin-brands.svg')}  />
+										<Image className="socialMedia" src={require('../images/linkedin-brands.svg')} alt="LinkedIn logo" />
 										<Button variant="link" href="https://www.linkedin.com/in/rehan-hajee/"><h4><b>LI/rehan-hajee</b></h4></Button>
 										<br />
 									</section>
 									<section>
 										<br />
-										<Image className="socialMedia" src={require('../images/email.svg')}  />
+										<Image className="socialMedia" src={require('../images/email.svg')} alt="Email" />
 										<Button variant="link" href="mailto:info@rehanhajee.me"><h4><b>Email</b></h4></Button>
 										<br />
 									</section>
 									<section>
 										<br />
 										
-										<Image className="socialMedia github" src={require('../images/github-brands.svg')}  />
+										<Image className="socialMedia github" src={require('../images/github-brands.svg')} alt="Github logo" />
 										<Button variant="link" href="https://github.com/rehanhajee/"><h4><b>Github/rehanhajee</b></h4></Button>
 										<br />
 									</section>

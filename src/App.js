a5871c0c2056532bee220de1bb70ee63df6231e1
Rehan Hajee <rehanhajee@gmail.com>
2022-06-25 22:20:22 -0400

    Added Particles background

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom"; 
+import Particles from 'react-particles-js';
 import CustomNavbar from './components/CustomNavbar';
 import Footer from './components/Footer';
 import Home from './components/Home';
@@ -8,6 +9,8 @@ import Programming from './components/Programming';
 import Media from './components/Media';
 import Contact from './components/Contact';
 import Error from './components/Error';
+import Test from './components/Test';
+
 
 import './App.css';
 
@@ -17,13 +20,44 @@ class App extends Component {
 			<main>
 				<BrowserRouter>
 					<article>
-						<CustomNavbar />
+					<CustomNavbar />
+					<Particles
+						params={{ 
+							"particles": {
+								"line_linked": {
+									"color":"#FFFFFF"
+								},
+								"number": {
+									"value": 100
+								},
+								"size": {
+									"value": 5
+								}
+							},
+							"interactivity": {
+								"detect_on": "window",
+								"events": {
+									"onhover": {
+										"enable": true, 
+										"mode": "repulse"
+									}
+								}
+							}
+						}}
+						style={{
+							width: '100%',
+							zIndex: '-999', 
+							position: 'fixed',
+							background: '#7a7a7a'
+						}} 
+						/>
 						<Switch className="App">
 							<Route exact path="/" component={Home}/>
 							<Route exact path="/About" component={About}/>
 							<Route path="/Programming" component={Programming}/>
 							<Route path="/Contact" component={Contact}/>
 							<Route path="/Media" component={Media}/>
+							<Route path="/Test" component={Test}/>
 							<Route path="" component={Error}/>
 						</Switch>
 						<Footer />
